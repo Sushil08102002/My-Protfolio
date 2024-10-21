2a7d7f346ede6de0c2f718010fd765607469d9d2
sushil lodhi <sushillodhi671@gmail.com>
2024-10-21 09:48:25 +0530

    This is my first commit

---
+++ src/data/constants.js
@@ -0,0 +1,244 @@
+export const Bio = {
+  name: "Sushil Lodhi",
+  roles: [
+    "Python Developer",
+    "Front-End Developer",
+    "Software Tester",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Sushil08102002",
+  resume:
+    "https://drive.google.com/file/d/1XG1P-bQJ7hsY03zESWmP67XX5wfUU0t7/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sushil-lodhi-653a4b208/",
+  insta: "https://www.instagram.com/sushil5134/",
+  facebook: "https://www.facebook.com/sushil.lodhi.353",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "Django",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlFFyY16N5NRhzoG24RqB7x8Ok2t8Bdgs-tuMsOBXS2Q&s",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      }
+    ],
+  },
+  {
+    title: "Software Testing",
+    skills: [
+      {
+        name: "Manual Testing",
+        image:
+          "https://img.icons8.com/?size=100&id=12356&format=png&color=000000",
+      },
+      {
+        name: "Selenium Webdriver",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Selenium_Logo.png/220px-Selenium_Logo.png",
+      },
+      {
+        name: "Jmeter",
+        image:"https://www.tatvasoft.com/blog/wp-content/uploads/2016/03/guidelines-for-software-performance-testing-with-apache-jmeter-1-768x389.jpg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://logowik.com/content/uploads/images/postman-api-platform6643.logowik.com.webp",
+      },
+      {
+        name: "WebSocket",
+        image: "https://adroitgroup-io-storage.s3.eu-central-1.amazonaws.com/blog/2023-05-31_EQ4jqfcbWY_the-power-of-websocket-revolutionizing-real-time-communication-on-theweb-bannerImg.png",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Vercel",
+        image:"https://cdn.worldvectorlogo.com/logos/vercel.svg",
+      },
+      {
+        name: "Linear",
+        image:"https://pageflows.com/media/logos/linear.png",
+      },
+      {
+        name: "Workflow Automation",
+        image:"https://thumbs.dreamstime.com/b/workflow-icon-white-background-flat-style-gear-arrow-ic-workflow-icon-white-background-flat-style-gear-arrow-icon-130737945.jpg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt5tMZxcK71cgavcdpgWPb5gaed6rgyS6jfw&s",
+    role: "Quality Analyst Intern",
+    company: "Walkover Web Solutions Pvt. Ltd.",
+    date: "October 2023 - July 2024",
+    desc: "As the team lead, I successfully managed our team while performing manual testing, writing automation scripts in Selenium, and integrating third-party applications into our product. I also focused on API testing, utilizing an automation framework, conducting performance monitoring, performing root cause analysis, and ensuring effective stakeholder communication throughout the process.",
+    skills: [
+      "Web Development",
+      "Test Plan and Test Strategy",
+      "Manual Testing",
+      "Automation Testing",
+      "Selenium Webdriver",
+      "Postman and API Testing",
+      "Web Socket",
+      "Third-party Integration"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1696514649120.jpeg?alt=media&token=e7f6757b-edfa-4138-a692-d6709eeef3e2",
+  },
+  {
+    id: 1,
+    img: "https://logowik.com/content/uploads/images/persistent-systems4900.logowik.com.webp",
+    role: "Persistent Martian Intern",
+    company: "Persistent",
+    date: "June 2023 - July 2023",
+    desc: "Learned about Python, DSA, DBMS, Operating System. Complete hands-on assessments, tasks, Attend lectures and practice sessions ",
+    skills: [
+      "Python",
+      "DBMS",
+      "MySQL",
+      "Linux",
+      "DSA"
+    ] }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/e/e6/LNCT_Bhopal_Logo.png",
+    school: "Lakshmi Narain College of Technology & Science, Bhopal",
+    date: "Oct 2020 - July 2024",
+    grade: "8.83 CGPA",
+    desc: "I completed my graduation in year 2024 with ECE branch. During my graduation I learned concepts of both Core Electronics concept and Computer fundamentals and programming",
+    degree: "Bachelor of Technology - BTech, Electronics & Communication Engineering (ECE)",
+  },
+  {
+    id: 1,
+    img: "https://ssvmdewas.org/wp-content/themes/school/images/new_logo.png",
+    school: "Saraswati Vidya Mandir, Dewas",
+    date: "June 2019 - July 2020",
+    grade: "85.8%",
+    desc: "I completed my class 12 high school education at Saraswati Vidya Mandir, Dewas, where I studied PCM with optional subject of Hindi",
+    degree: "HSC(XII), PCM",
+  },
+  {
+    id: 2,
+    img: "https://ssvmdewas.org/wp-content/themes/school/images/new_logo.png",
+    school: "Saraswati Vidya Mandir, Dewas",
+    date: "June 2017 - July 2018",
+    grade: "87.4%",
+    desc: "I completed my class 10 education at Saraswati Vidya Mandir, Dewas, where I studied Science with English and Hindi.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Blog Website",
+    date: "Jan 2024- Feb 2024",
+    description: "My blog website, built using Django, offers a user-friendly platform for sharing insightful articles and engaging content. It features a clean design, intuitive navigation, and interactive commenting to foster reader interaction and community building.",
+    image: "https://www.sourcecodester.com/sites/default/files/images/oretnom23/python_blog_home.png",
+    tags: [
+      "Django",
+      "Python",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "JavaS",
+    ],
+    category: "web app",
+    webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 10,
+    title: "Virtual Attendence System",
+    date: "June 2023- August 2023",
+    description: "My Virtual Attendance System, developed with Python, provides a reliable and efficient solution for tracking and managing attendance. It utilizes facial recognition technology and intuitive user interfaces to ensure accurate and seamless attendance recording for educational and professional environments.",
+    image: "https://i.ytimg.com/vi/hT4zp-mGFkE/hqdefault.jpg",
+    tags: [
+      "Python",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Natural Language Processing",
+      "Pandas"
+    ],
+    category: "python",
+    webapp: "https://decisionhub.netlify.app/",
+  }
+];
+
+
