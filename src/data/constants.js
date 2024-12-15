f0796071519def58b7de56e08be44ca90f2d803c
sushil lodhi <sushillodhi671@gmail.com>
2024-12-15 23:30:20 +0530

    photo and resume changed

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Sushil08102002",
   resume:
-    "https://drive.google.com/file/d/1XG1P-bQJ7hsY03zESWmP67XX5wfUU0t7/view?usp=sharing",
+    "https://drive.google.com/file/d/1AV3oF0G4xlcspR_V5Lwve2rBV2uVKfMd/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sushil-lodhi-653a4b208/",
   insta: "https://www.instagram.com/sushil5134/",
   facebook: "https://www.facebook.com/sushil.lodhi.353",
